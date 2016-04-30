22296d13b37bff7e38e329abc9a3e258d8193df5
gautierenaud <gautierenaud@gmail.com>
2016-04-30 11:40:13 +0200

    add frameself library to the android project

--- SmartControl/app/src/main/java/toulouse/insa/smartcontrol/viewactivities/CreateRule.java
+++ SmartControl/app/src/main/java/toulouse/insa/smartcontrol/viewactivities/CreateRule.java
@@ -2,10 +2,9 @@ package toulouse.insa.smartcontrol.viewactivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
-
-import java.util.List;
 
 import toulouse.insa.smartcontrol.R;
 import toulouse.insa.smartcontrol.common.FrameselfObject;
@@ -16,10 +15,16 @@ public class CreateRule extends AppCompatActivity {
     private EditText mEditTrigger;
     private EditText mEditAction;
 
+    private frameself.format.Action action;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_rule);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
         this.setTitle("Create New Rule");
 
         mEditTitle = (EditText) findViewById(R.id.edit_title);
