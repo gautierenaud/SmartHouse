c3824d9f767ac10f14e5121f7b3ac59d5d1893fb
gautierenaud <gautierenaud@gmail.com>
2016-05-15 21:51:33 +0200

    adjust paramNum for gui option in frameself

--- FrameselfManager/src/launcher/Main.java
+++ FrameselfManager/src/launcher/Main.java
@@ -20,12 +20,12 @@ public class Main {
 	 
 	 *actionner : 127.0.0.1
 	 *portGetAction (actionner) : 9000
-	 *portSendResult (actionner) : 10000
+	 *portSendResult (action0ner) : 10000
 	 */
 	public static void main(String[] args) 
 	{
 		
-		Launcher launcher = new Launcher(args.length == 7);
+		Launcher launcher = new Launcher(args.length == 8);
 		ServerFrameself serverFrameself = new ServerFrameself("127.0.0.1", Integer.parseInt(args[0]), Integer.parseInt(args[1]));
 		ServerAndroid serverAndroid = new ServerAndroid(launcher, Integer.parseInt(args[2]));
 		ServerActionner serverActionner = new ServerActionner(args[3], Integer.parseInt(args[4]), Integer.parseInt(args[5]));
