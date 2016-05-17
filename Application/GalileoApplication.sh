a7b884b6d4d486ac8bbca2858df3ccf767aa5d71
PeaceMaker503 <chellal-halim@hotmail.fr>
2016-05-17 22:14:18 +0200

    co

--- Application/GalileoApplication.sh
+++ Application/GalileoApplication.sh
@@ -1,1 +1,1 @@
-java -jar GalileoApplication.jar 192.168.41.1 5000 192.168.41.1 9000 10000 /dev/ttyACM0 9600
+java -jar GalileoApplication.jar 192.168.41.1 5000 192.168.41.1 9000 10000 /dev/ttyACM0 230400
